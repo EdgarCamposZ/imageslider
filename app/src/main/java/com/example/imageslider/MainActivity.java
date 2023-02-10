5e6bfb4234f8e900103a0e4841437c8998ba600d
Edgar Campos <camposedgar899@gmail.com>
2023-02-09 21:15:01 -0600

    Initial commit

---
+++ app/src/main/java/com/example/imageslider/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.imageslider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+public class MainActivity extends AppCompatActivity {
+
+    SliderView sliderView;
+    int[] images = {R.drawable.one,
+    R.drawable.two,
+    R.drawable.three,
+    R.drawable.four,
+    R.drawable.five,
+    R.drawable.six};
+
+    Button web;
+    String url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+
+        url = "https://strangerthings-experience.com/es/https://strangerthings-experience.com/es/";
+        web= findViewById(R.id.btnWeb);
+
+        web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+    }
+}
